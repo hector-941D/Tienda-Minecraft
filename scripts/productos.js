d2ea50ea3e41960890b673a2b587b2b66313cb51
hector-941D <hec.villacura@duocuc.cl>
2026-09-10 15:53:16 -0300

    Scripts y inyecciones
    Se agregaron scripts que contienen inyecciones y se cambio un par de cosas mas

--- scripts/productos.js
+++ scripts/productos.js
@@ -1,7 +1,7 @@
 let productoss = [
-  {id: 1, img:"../images/Paja.webp", nombre: "paja xd", precio: 2},
-  {id: 2, img:"../images/PicoHierro.webp", nombre: "Pico Hierro", precio: 4},
-  {id: 3, img:"../images/Torta.webp", nombre: "Torta", precio: 3},
+  {id: 1, img:"../images/PicoHierro.webp", nombre: "Pico Hierro", precio: 4},
+  {id: 2, img:"../images/Paja.webp", nombre: "paja ", precio: 2},
+  {id: 3, img:"../images/Torta.webp", nombre: "Torta", precio: 1},
 ];
 
 function mostrarProductos(){
@@ -34,4 +34,14 @@ function mostrarProducto() {
    </div>
   `;
  }
+}
+
+function agregarProductoPi(){
+  document.getElementById("productosPi").innerHTML = "<div class='producto' id='productoPi'><h2>Pico de Hierro</h2><img src='../images/PicoHierro.webp' width='120' alt='Pico'><p>Precio: 4 <img src='../images/Esmeraldas.webp' width='20' alt='Esmeraldas'></p><button>Quitar</button></div>";
+}
+function agregarProductoPa(){
+  document.getElementById("productosPa").innerHTML = "<div class='producto' id='productoPa'><h2>Paja</h2><img src='../images/Paja.webp' width='120' alt='Paja'><p>Precio: 2 <img src='../images/Esmeraldas.webp' width='20' alt='Esmeraldas'></p><button>Quitar</button></div>";
+}
+function agregarProductoTo(){
+  document.getElementById("productosTo").innerHTML = "<div class='producto' id='productoTo'><h2>Torta</h2><img src='../images/Torta.webp' width='120' alt='Torta'><p>Precio: 1 <img src='../images/Esmeraldas.webp' width='20' alt='Esmeraldas'></p><button>Quitar</button></div>";
 }
---
+++ scripts/registro.js
@@ -0,0 +1,61 @@
+function obtenerUsuario(){
+    let nombre = document.getElementById("nombre");
+    if(validarString(nombre.value, 2, 20)){
+        nombre.classList.remove("border-red");
+        return nombre;
+    }else{
+        alert("nombre no válido");
+        nombre.classList.add("border-red");
+        nombre.focus();
+        return null;
+    }
+}
+
+function obtenerContraseña(){
+    let contraseña = document.getElementById("contraseña");
+    if(validarString(contraseña.value, 8, 30)){
+        contraseña.classList.remove("border-red");
+        return contraseña;
+    }else{
+        alert("contraseña no válida");
+        contraseña.classList.add("border-red");
+        contraseña.focus();
+        return null;
+    }
+}
+
+function obtenerCorreo(){
+    let correo = document.getElementById("correo");
+    if(validarString(correo.value, 12, 50)){
+        correo.classList.remove("border-red");
+        return correo;
+    }else{
+        alert("correo no válido");
+        correo.classList.add("border-red");
+        correo.focus();
+        return null;
+    }
+}
+function validarString(text, min, max){
+    if (typeof text !== "string") {
+        return false;
+    }
+    let trimmedText = text.trim();
+    if(trimmedText.length >= min && trimmedText.length <= max){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validarCorreo(text, min, max){
+    if (typeof text !== "string") {
+        return false;
+    }
+    let trimmedText = text.trim();
+    if(trimmedText.length >= min && trimmedText.length <= max){
+        return true;
+    }else{
+        return false;
+    }
+}
